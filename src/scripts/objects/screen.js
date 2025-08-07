927014d83aaf8988d7bb7aa5f6ba37efce42c937
Renato S. A. <renatotato.rsa@gmail>
2025-08-07 13:44:09 -0300

    Solicitaçao 3 concluida;

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -2,23 +2,36 @@ const screen = {
     userPorfile: document.querySelector(".profile-data"),
     RenderUser(user) {
         this.userPorfile.innerHTML += `<div class="info">
-                                                                <img src="${user.foto}" alt="Imagem de perfil do usuário.">
-                                                                <div class="data">
-                                                                    <h1>${user.nome ?? "Informação não existente"}</h1>
-                                                                    <p>Nome de Usuário: ${user.nomeUsuario ?? "Informação não existente"}</p>
-                                                                    <p>Bio: ${user.bio ?? "Informação não existente"}</p>
-                                                                    <p>Email: ${user.email ?? "Informação não existente"}</p>
-                                                                    <p>Número de seguidores: ${user.seguidores ?? "Informação não existente"}</p>
-                                                                    <p>Número de pessoas que segue: ${user.seguindo ?? "Informação não existente"}</p>
-                                                                </div>`
+                                            <img src="${user.foto}" alt="Imagem de perfil do usuário.">
+                                            <div class="data">
+                                                <h1>${user.nome ?? "Informação não existente"}</h1>
+                                                <p>Nome de Usuário: ${user.nomeUsuario ?? "Informação não existente"}</p>
+                                                <p>Bio: ${user.bio ?? "Informação não existente"}</p>
+                                                <p>Email: ${user.email ?? "Informação não existente"}</p>
+                                                <p>Número de seguidores: ${user.seguidores ?? "Informação não existente"}</p>
+                                                <p>Número de pessoas que segue: ${user.seguindo ?? "Informação não existente"}</p>
+                                            </div>
+                                        </div>`
         if (user.repositorios.length > 0) {
             let listaRepositorios = "";
             user.repositorios.forEach(repositorio => {
-                listaRepositorios += `<li><a href="${repositorio.html_url}" target="_blank">${repositorio.name}</li>`
+                let forks = repositorio.forks_count;
+                let stargazers = repositorio.stargazers_count;
+                let watchers = repositorio.watchers_count;
+                let language = repositorio.language;
+                listaRepositorios += `<li><a href="${repositorio.html_url}" target="_blank">
+                                        <h4>${repositorio.name}</h4>
+                                        <span>🍴${forks}</span>
+                                        <span>⭐${stargazers}</span>
+                                        <span>👀${watchers}</span>
+                                        <span>⌨️${language ?? "Não informado"}</span>
+                                    </a><li>`
+                console.log(repositorio)
             });
             this.userPorfile.innerHTML += `<div class="repositories section">
-                                                                <h2>Repositórios</h2>
-                                                                <ul>${listaRepositorios}</ul>`
+                                                <h2>Repositórios</h2>
+                                                <ul>${listaRepositorios}</ul>
+                                            </div>`
         }
         if (user.eventos.length > 0) {
             let listaEventos = "";
@@ -33,8 +46,9 @@ const screen = {
                 listaEventos += `<li><p class="name">${nomeEvento}</p><p class="commit">-${commit}</p></li>`;
             });
             this.userPorfile.innerHTML += `<div class="events section">
-                                                                <h2>Eventos</h2>
-                                                                <ul>${listaEventos}</ul>`
+                                                <h2>Eventos</h2>
+                                                <ul>${listaEventos}</ul>
+                                            </div>`
         }
     },
     RenderNotFound() {
